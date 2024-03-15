f33bb16d19afbd147398b25935637b48910fcec6
Maxiee <maxieewong@gmail.com>
2024-03-15 13:02:49 +0800

    功能跑通

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Notice, Plugin, TFile } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
@@ -13,122 +13,64 @@ const DEFAULT_SETTINGS: MyPluginSettings = {
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
 
+	async weaveKnowledgeInternal(content: string, activeFile: TFile, maxDepth = 5, currentDepth = 0): Promise<string> {
+		if (currentDepth > maxDepth) {
+		  throw new Error('Max embed depth exceeded');
+		}
+	  
+		const embedRegex = /!\[\[([^|\]]+)(?:\|([^\]]+))?\]\]/g;
+		let match;
+		let lastIndex = 0;
+	  
+		while ((match = embedRegex.exec(content)) !== null) {
+		  const embedPath = match[1];
+		  const embedFile = this.app.metadataCache.getFirstLinkpathDest(embedPath, activeFile.path);
+		  if (embedFile) {
+			let embedContent = await this.app.vault.cachedRead(embedFile);
+			embedContent = await this.weaveKnowledgeInternal(embedContent, activeFile, maxDepth, currentDepth + 1);
+			content = content.slice(0, match.index) + embedContent + content.slice(match.index + match[0].length);
+			lastIndex = match.index + embedContent.length;
+		  } else {
+			new Notice(`Unable to resolve embed: ${embedPath}`);
+			lastIndex = match.index + match[0].length;
+		  }
+	  
+		  embedRegex.lastIndex = lastIndex;
+		}
+	  
+		return content;
+	}
+
+	async weaveKnowledge() {
+		const activeFile = this.app.workspace.getActiveFile();
+		if (!activeFile) {
+		  new Notice('No active file.');
+		  return;
+		}
+	  
+		let fileContent = await this.app.vault.cachedRead(activeFile);
+		fileContent = await this.weaveKnowledgeInternal(fileContent, activeFile);
+		
+		await navigator.clipboard.writeText(fileContent);
+		new Notice('Knowledge weaved and copied to clipboard!');
+	}
+
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+			id: 'weave-knowledge',
+			name: "Weave Knowledge",
+			callback: () => this.weaveKnowledge()
+		})
 	}
 
 	onunload() {
 
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+	async loadSettings() {}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+	async saveSettings() {}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
-}
